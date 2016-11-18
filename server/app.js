a0ba9a03dee5062c053520070fbda6fdcc4caf3e
r45r54r45 <r45r54r45@yonsei.ac.kr>
2016-11-18 23:45:02 +0900

    ff

--- server/app.js
+++ server/app.js
@@ -106,15 +106,16 @@ app.post('/item',upload.array('image'), function (req, res) {
 });
 app.put('/item',upload.array('image'), function (req, res) {
     var db = require('./mysql');
-    let itemId=req.query.id;
+    var itemId=req.query.id;
+    var data=req.body;
     req.body.deleteImageList.split(',').forEach(function(item, index){
         db.query('delete from Image where id=?',[item])
     })
     db.query('update Item set ' +
         ' type=?, title=?, location=?, produced_area=?, real_area=?, floor=?, total_floor=?, room=?, toilet=?, specification=?, available=?, j_price=?, m_price=?, b_price=?, w_price=?)' +
         ' where id=?',[data.type, data.title, data.location, data.produced_area, data.real_area, data.floor, data.total_floor, data.room, data.toilet, data.specification, data.available, data.j_price||null, data.m_price||null, data.b_price||null, data.w_price||null, itemId],function(err, result){
-        for(let key in req.files){
-            let dat=req.files[key];
+        for(var key in req.files){
+            var dat=req.files[key];
             db.query("insert into Image (url, item) values (?,?)",[dat.filename,itemId],function(err, result){
 
             })
