8b4db133ac4dd37dd0769187e3ef5c4c7f38c31a
r45r54r45 <r45r54r45@yonsei.ac.kr>
2016-11-19 16:27:10 +0900

    ff

--- src/container/ViewScreen/index.js
+++ src/container/ViewScreen/index.js
@@ -17,79 +17,24 @@ class ViewScreen extends React.Component {
             current: {
                 data: {}
             },
-            data: [
-                {
-                    type: "매매",
-                    name: "송도동 센트럴파크 3차",
-                    location: '인천광역시 연수구 송도2동',
-                    producedArea: 108.21,
-                    realArea: 84.97,
-                    floor: 5,
-                    totalFloor: 10,
-                    room: 5,
-                    toilet: 3,
-                    dueDate: '1개월 이내',
-                    specification: '남쪽으로 센트럴파크 공원',
-                    price: 69000,
-                    created_at: '2016년 9월 5일',
-                    image: [
-                        'http://landthumb.phinf.naver.net/20161110_277/atcl_photo_1478740372135OnIXk_JPEG/1e8b46d88a0281bb9eedff6ed362b763.JPG?type=m562',
-                        'http://landthumb.phinf.naver.net/20161110_277/atcl_photo_1478740372212rAkCD_JPEG/99dc2e3fec8f1715a38f4524f2c1dae7.JPG?type=m562',
-                        // 'http://landthumb.phinf.naver.net/20161110_256/atcl_photo_14787403723479PiGA_JPEG/1f329c804207c0d49546d687e41298dc.JPG?type=m562',
-                        // 'http://landthumb.phinf.naver.net/20161110_166/atcl_photo_1478740372129lntOw_JPEG/bfc9011d640d76c1cfb94a08fb21532f.JPG?type=m562',
-                        // 'http://landthumb.phinf.naver.net/20161110_267/atcl_photo_1478740372229qyURE_JPEG/b715dfcdc3aceff8e3bbf554de14b212.JPG?type=m562'
-                    ]
-
-                },
-                {
-                    type: "월세",
-                    name: "송도동 센트럴파크 2차",
-                    location: '인천광역시 옥련동',
-                    producedArea: 38.21,
-                    realArea: 20.97,
-                    floor: 40,
-                    totalFloor: 43,
-                    room: 4,
-                    toilet: 2,
-                    dueDate: '3일 이내',
-                    specification: '주변에 조용한 거리',
-                    price: 43000,
-                    created_at: '2016년 5월 2일',
-                    image: [
-                        'http://landthumb.phinf.naver.net/20161110_14/atcl_photo_1478767285515Hd5WA_JPEG/eae864104a2b6c5b9622ec18d594473e.JPG?type=m562',
-                        'http://landthumb.phinf.naver.net/20161110_112/atcl_photo_1478767285528p60FV_JPEG/cb2fdce48cf21abb999afe867f39aa27.JPG?type=m562',
-                        'http://landthumb.phinf.naver.net/20161110_18/atcl_photo_1478767285526YieVL_JPEG/09fad2096756572e4d42025e45115429.JPG?type=m562',
-                        'http://landthumb.phinf.naver.net/20161110_59/atcl_photo_1478767285529oUdmO_JPEG/be6fb99b8d068d11e96981d0e31a304a.JPG?type=m562'
-                    ]
-                },
-                {
-                    type: "전세",
-                    name: "송도동 센트럴파크 1차",
-                    location: '인천광역시 연수구 송도2동',
-                    producedArea: 198.21,
-                    realArea: 134.97,
-                    floor: 1,
-                    totalFloor: 3,
-                    room: 2,
-                    toilet: 1,
-                    dueDate: '1년 이내',
-                    specification: '비행기가 위로 지나다님',
-                    price: 29000,
-                    created_at: '2016년 9월 2일',
-                    image: [
-                        'http://landthumb.phinf.naver.net/20161110_21/atcl_photo_14787845303455vL59_JPEG/fd34cbafe8f1fe751715bc78006c55f8.JPG?type=m562',
-                        'http://landthumb.phinf.naver.net/20161110_215/atcl_photo_1478784530345sXnA9_JPEG/36f4334d14e1823a82dcf54b506481de.JPG?type=m562',
-                        'http://landthumb.phinf.naver.net/20161110_166/atcl_photo_1478784530348zphmR_JPEG/f5215917a3c49a583dfe0bc153d14b30.JPG?type=m562'
-                    ]
-                }
-            ]
+            basic: {},
+            data: []
         }
 
     }
 
+    componentWillMount() {
+        fetch('/view?id=' + localStorage.getItem("userId")).then(dat=>dat.json()).then(data=> {
+            this.setState({
+                data: data.list,
+                basic: data.basic
+            })
+        })
+    }
+
     componentDidMount() {
         this.computeLists();
-        this.timeInterval=700;
+        this.timeInterval = 4000;
     }
 
     computeLists() {
@@ -98,9 +43,9 @@ class ViewScreen extends React.Component {
         let slideBreakPoint = [0];
         let slideList = [];
         for (let i = 0; i < list.length; i++) {
-            let length = list[i].image.length;
+            let length = list[i].images.length;
             slideBreakPoint.push(slideBreakPoint[slideBreakPoint.length - 1] + length);
-            slideList = [...slideList, ...list[i].image]
+            slideList = [...slideList, ...list[i].images]
         }
         this.setState(
             {
@@ -125,18 +70,50 @@ class ViewScreen extends React.Component {
              -> 매물 몇개 마다
              -> 예를 들어 매물이 3개여도 5개 마다 재생되도록 (반복 되니까)
              */
-            const jugi = 4;
+            const jugi_video = this.state.basic.video_num;
+            const jugi_image= this.state.basic.image_num;
+            var videoEnable=false;
+            var imageEnable=false;
+            if(jugi_video==-1&&jugi_image==-1){
+                resolve();
+            }
+            if(!((jugi_video!==-1&&this.state.basic.video_id!==null)||(jugi_image!==-1&&this.state.basic.image_url!==null))){
+                resolve();
+            }
+            if(jugi_video!==-1&&this.state.basic.video_id!==null){
+                videoEnable=true;
+            }
+            if(jugi_image!==-1&&this.state.basic.image_url!==null){
+                imageEnable=true;
+            }
             let nextCount = this.state.currentCount + 1;
             this.setState({
                 currentCount: nextCount
             })
-            console.info("현재 재생 번호: ", nextCount);
-            if (nextCount % 4 !== 0) { //아직 fire 할때가 아니라면
+            function checkRotation(videoEnable, jugi_video, imageEnable, jugi_image, nextCount){
+                if(videoEnable){
+                    if(nextCount % jugi_video === 0){
+                        return 'VIDEO'
+                    }
+                }
+                //비디오 우선
+                if(imageEnable){
+                    if(nextCount % jugi_image === 0){
+                        return 'IMAGE'
+                    }
+                }
+                return false;
+            }
+            var rotation=checkRotation(videoEnable, jugi_video, imageEnable, jugi_image, nextCount);
+            if (!rotation) { //아직 fire 할때가 아니라면
                 resolve();
-            } else { //비디오 재생할 때라면
+            } else if(rotation==='VIDEO'){ //비디오 재생할 때라면
+                var w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+                var h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
                 var options = {
-                    id: 192222354,
-                    width: 900
+                    id: this.state.basic.video_id,
+                    width: w,
+                    height: h
                 };
                 this.setState({
                     videoPlay: true
@@ -153,6 +130,11 @@ class ViewScreen extends React.Component {
                         })
                     });
                 })
+            }else if(rotation==="IMAGE"){
+                console.log("image");
+                resolve();
+            }else{
+                resolve();
             }
         });
     }
@@ -236,10 +218,10 @@ class ViewScreen extends React.Component {
                                 <div className="first-row">
                                     <div className="left">
                                         <span className="type">
-                                            {currentData.type}
+                                            {currentData.type==1?"전세":(currentData.type==2?"매매":"월세")}
                                         </span>
                                         <span className="desc">
-                                            {currentData.name}
+                                            {currentData.title}
                                         </span>
                                     </div>
                                     <div className="right">
@@ -247,7 +229,7 @@ class ViewScreen extends React.Component {
                                             <span>공급/전용면적</span><span>(전용률 {percent}%)</span>
                                         </div>
                                         <div className="desc">
-                                            <span>{currentData.producedArea}/{currentData.realArea}㎡</span>
+                                            <span>{currentData.produced_area}/{currentData.real_area}㎡</span>
                                         </div>
                                     </div>
                                 </div>
@@ -257,7 +239,7 @@ class ViewScreen extends React.Component {
                                             <span>해당층/총층</span>
                                         </div>
                                         <div className="desc">
-                                            <span>{currentData.floor}/{currentData.totalFloor}층</span>
+                                            <span>{currentData.floor}/{currentData.total_floor}층</span>
                                         </div>
 
                                     </div>
@@ -275,7 +257,7 @@ class ViewScreen extends React.Component {
                                         <span>입주가능일</span>
                                     </div>
                                     <div className="desc">
-                                        <span>{currentData.dueDate}</span>
+                                        <span>{currentData.available}</span>
                                     </div>
                                 </div>
                                 <div className="fourth-row">
@@ -290,7 +272,7 @@ class ViewScreen extends React.Component {
                             <div className="right-area">
                                 <div className="top">
                                     <div className="title">
-                                        {currentData.type}가
+                                        {currentData.type==1?"전세":(currentData.type==2?"매매":"월세")}가
                                     </div>
                                     <div className="desc">
                                         <span className="price">
@@ -332,17 +314,17 @@ class ViewScreen extends React.Component {
                                             </div>
                                             <div className="info-area">
                                                 <div className="first-row">
-                                                    <span className="type">{item.type}</span>
-                                                    <span className="title">{item.name}</span>
+                                                    <span className="type">{item.type==1?"전세":(item.type==2?"매매":"월세")}</span>
+                                                    <span className="title">{item.title}</span>
                                                 </div>
                                                 <div className="next-row">
                                                     <span className="left">{item.location}</span>
                                                     <span
-                                                        className="right">{item.realArea}㎡ {item.room}/{item.toilet}</span>
+                                                        className="right">{item.real_area}㎡ {item.room}/{item.toilet}</span>
                                                 </div>
                                                 <div className="next-row">
-                                                    <span className="left">세입자 고안매매</span>
-                                                    <span className="right">{item.price}만원</span>
+                                                    <span className="left">{item.specification}</span>
+                                                    <span className="right">{item.w_price}만원</span>
                                                 </div>
                                             </div>
                                         </div>
@@ -352,16 +334,16 @@ class ViewScreen extends React.Component {
                         </div>
                         <div className="store-info-area">
                             <div className="storeName">
-                                명문공인중개사
+                                {this.state.basic.store_name}
                             </div>
                             <div className="row">
-                                대표: 이인숙
+                                대표: {this.state.basic.ceo_name}
                             </div>
                             <div className="row">
-                                전화: 02-1234-1234
+                                전화: {this.state.basic.tel}
                             </div>
                             <div className="row">
-                                휴대폰: 010-1234-1234
+                                휴대폰: {this.state.basic.phone}
                             </div>
                             <div className="row">
                                 매물: {this.state.data.length}
@@ -374,7 +356,7 @@ class ViewScreen extends React.Component {
                     <img className="right" src="http://realty.mfamily.co.kr/images/footer_imgr.png"
                          role="presentation"/>
                     <div className="marquee-wrapper">
-                        <p className="marquee">안녕하세요</p>
+                        <p className="marquee">안녕하세요.  {this.state.basic.store_name} 입니다. </p>
                     </div>
                 </div>
             </div>
