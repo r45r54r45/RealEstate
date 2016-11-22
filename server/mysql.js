fa97806479de317354103a24aa22032f947ab4d1
r45r54r45 <r45r54r45@yonsei.ac.kr>
2016-11-22 21:23:30 +0900

    dd

--- server/mysql.js
+++ server/mysql.js
@@ -1,16 +1,16 @@
 var mysql      = require('mysql');
-// var connection = mysql.createConnection({
-//     host     : 'localhost',
-//     user     : 'root',
-//     password : 'e34e43E34!',
-//     database: 'db'
-// });
 var connection = mysql.createConnection({
     host     : 'localhost',
     user     : 'root',
-    password : '',
-    database: 'test'
+    password : 'e34e43E34!',
+    database: 'db'
 });
+// var connection = mysql.createConnection({
+//     host     : 'localhost',
+//     user     : 'root',
+//     password : '',
+//     database: 'test'
+// });
 connection.connect();
 
 module.exports= connection;
