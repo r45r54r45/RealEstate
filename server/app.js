1fb070fbdbea3f07cc3668aacf9adbd74031fed4
r45r54r45 <r45r54r45@yonsei.ac.kr>
2016-11-24 14:01:03 +0900

    d

--- server/app.js
+++ server/app.js
@@ -98,8 +98,8 @@ app.post('/item', upload.array('image'), function (req, res) {
     var data = req.body;
     if (req.query.id) {
         db.query('insert into Item ' +
-            ' (type, owner, title, location, produced_area, real_area, floor, total_floor, room, toilet, specification, available, j_price, m_price, b_price, w_price)' +
-            'values (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)', [data.type, req.query.id, data.title, data.location, data.produced_area, data.real_area, data.floor, data.total_floor, data.room, data.toilet, data.specification, data.available, data.j_price || null, data.m_price || null, data.b_price || null, data.w_price || null], function (err, result) {
+            ' (type, owner, title, location, produced_area, real_area, floor, total_floor, room, toilet, specification, available, j_price, m_price, b_price, w_price, y_price)' +
+            'values (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)', [data.type, req.query.id, data.title, data.location, data.produced_area, data.real_area, data.floor, data.total_floor, data.room, data.toilet, data.specification, data.available, data.j_price || null, data.m_price || null, data.b_price || null, data.w_price || null, data.y_price || null], function (err, result) {
             let id = result.insertId;
             for (let key in req.files) {
                 let dat = req.files[key];
@@ -129,8 +129,8 @@ app.put('/item', upload.array('image'), function (req, res) {
         db.query('delete from Image where id=?', [item])
     })
     db.query('update Item set ' +
-        ' type=?, title=?, location=?, produced_area=?, real_area=?, floor=?, total_floor=?, room=?, toilet=?, specification=?, available=?, j_price=?, m_price=?, b_price=?, w_price=? ' +
-        ' where id=?', [toInt(data.type), data.title, data.location, toFloat(data.produced_area), toFloat(data.real_area), toInt(data.floor), toInt(data.total_floor), toInt(data.room), toInt(data.toilet), data.specification, data.available, data.j_price || null, data.m_price || null, data.b_price || null, data.w_price || null, toInt(itemId)], function (err, result) {
+        ' type=?, title=?, location=?, produced_area=?, real_area=?, floor=?, total_floor=?, room=?, toilet=?, specification=?, available=?, j_price=?, m_price=?, b_price=?, w_price=?, y_price=? ' +
+        ' where id=?', [toInt(data.type), data.title, data.location, toFloat(data.produced_area), toFloat(data.real_area), toInt(data.floor), toInt(data.total_floor), toInt(data.room), toInt(data.toilet), data.specification, data.available, data.j_price || null, data.m_price || null, data.b_price || null, data.w_price || null, data.y_price || null, toInt(itemId)], function (err, result) {
         if (err) {
             res.json(err);
         }
